4ca105a98a2637afcb341fb9f71aad71e8bbcf7c
Syed Zaidi <53128757+syedzaidi-magento@users.noreply.github.com>
2019-10-25 23:41:32 -0700

    build with json

---
+++ build/precache-manifest.4ce790988a31fbb05a21cd9ef11e92bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4737b4aaab009c13d66208244e09f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8f67bc451d29f483b46",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "f206294d7cd10d47e0e6",
+    "url": "/static/css/main.aac503a4.chunk.css"
+  },
+  {
+    "revision": "f8f67bc451d29f483b46",
+    "url": "/static/js/2.622382c5.chunk.js"
+  },
+  {
+    "revision": "f206294d7cd10d47e0e6",
+    "url": "/static/js/main.e06ee3df.chunk.js"
+  },
+  {
+    "revision": "6a193a9c29f7a6fbcfb4",
+    "url": "/static/js/runtime~main.fc6c27ef.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
